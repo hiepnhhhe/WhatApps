86661cbb004fec1143783bb639baedab78a4ac5f
hiepnhhhe <hiepnhhhe130170@fpt.edu.vn>
2020-07-03 17:44:06 +0700

    Login, Registration view.

--- app/src/main/java/com/example/whatapps/MainActivity.java
+++ app/src/main/java/com/example/whatapps/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.whatapps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabAccessorAdapter;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WhatsApp");
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabAccessorAdapter);
+
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (currentUser == null){
+            SendUserToLoginActivity();
+        }
+        
+    }
+
+    private void SendUserToLoginActivity(){
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
